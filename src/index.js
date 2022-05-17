370d1d10457c2cf96c37c43f304fe07db23df08b
PapayaHUANG <hsy922@qq.com>
2022-05-17 21:48:55 +0800

    add version and routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
+const v1WorkoutRouter = require('./v1/routes/workoutRoutes');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send("<h2>It's Working!</h2>");
-});
+app.use('/api/v1/workouts', v1WorkoutRouter);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
