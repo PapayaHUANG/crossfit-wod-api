a6def411c5a18db55d3ef1a9d2440842163c8295
PapayaHUANG <hsy922@qq.com>
2022-05-18 14:14:38 +0800

    inserted json file

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,27 @@
+const workoutService = require('../services/workoutService');
+
 const getAllWorkouts = (req, res) => {
-  res.send('Get all workouts');
+  const allWorkouts = workoutService.getAllWorkouts();
+  res.send({ status: 'OK', data: allWorkouts });
 };
 
 const getOneWorkout = (req, res) => {
+  const workout = workoutService.getOneWorkout();
   res.send('Get an existing workout');
 };
 
 const createNewWorkout = (req, res) => {
+  const createWorkout = workoutService.createNewWorkout();
   res.send('Create a new workout');
 };
 
 const updateOneWorkout = (req, res) => {
+  const updateWorkout = workoutService.updateOneWorkout();
   res.send('Update an existing workout');
 };
 
 const deleteOneWorkout = (req, res) => {
+  workoutService.deleteOneWorkout();
   res.send('Delete an existing workout');
 };
 
---
+++ src/services/workoutService.js
@@ -0,0 +1,30 @@
+const Workout = require('../database/Workout');
+
+const getAllWorkouts = () => {
+  const allWorkouts = Workout.getAllWorkouts();
+  return allWorkouts;
+};
+
+const getOneWorkout = () => {
+  return;
+};
+
+const createNewWorkout = () => {
+  return;
+};
+
+const updateOneWorkout = () => {
+  return;
+};
+
+const deleteOneWorkout = () => {
+  return;
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
