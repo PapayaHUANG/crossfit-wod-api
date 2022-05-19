00da363d7c93431c71ea0e9f9b86cb9ec0807d30
PapayaHUANG <hsy922@qq.com>
2022-05-19 19:49:47 +0800

    integrating filter

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,8 +1,9 @@
 const workoutService = require('../services/workoutService');
 
 const getAllWorkouts = (req, res) => {
+  const { mode } = req.query;
   try {
-    const allWorkouts = workoutService.getAllWorkouts();
+    const allWorkouts = workoutService.getAllWorkouts({ mode });
     res.send({ status: 'OK', data: allWorkouts });
   } catch (error) {
     res.status(error?.status || 500).send({
--- src/database/Workout.js
+++ src/database/Workout.js
@@ -2,9 +2,15 @@ const DB = require('./db.json');
 
 const { saveToDatabase } = require('./utils');
 
-const getAllWorkouts = () => {
+const getAllWorkouts = (filterParams) => {
   try {
-    return DB.workouts;
+    let workouts = DB.workouts;
+    if (filterParams.mode) {
+      return DB.workouts.filter((workout) =>
+        workout.mode.toLowerCase().includes(filterParams.mode)
+      );
+    }
+    return workouts;
   } catch (error) {
     throw { status: 500, message: error };
   }
