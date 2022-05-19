64ad364f0be9b63dedabe084ef67272dae23d77d
PapayaHUANG <hsy922@qq.com>
2022-05-19 20:14:35 +0800

    add cache

--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const apicache = require('apicache');
+
 const workoutController = require('../../controllers/workoutController');
 const recordController = require('../../controllers/recordController');
 
 const router = express.Router();
 
-router.get('/', workoutController.getAllWorkouts);
+const cache = apicache.middleware;
+
+router.get('/', cache('2 minutes'), workoutController.getAllWorkouts);
 
 router.get('/:workoutId', workoutController.getOneWorkout);
 
